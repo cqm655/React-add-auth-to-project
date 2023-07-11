faebd64d06d87ada3a4958cab3658f28abc05950
iurie.balan <iurie.balan@112.md>
2023-07-11 14:32:59 +0300

    added logout in React style

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Form } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
@@ -44,6 +44,11 @@ function MainNavigation() {
               }>
               Authentification
             </NavLink>
+          </li>
+          <li>
+            <Form action="/logout" method="POST">
+              <button>Logout</button>
+            </Form>
           </li>
         </ul>
       </nav>
